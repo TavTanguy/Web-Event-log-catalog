e6e023de0c5d34b7e11dd646760a29093c9aeb6b
GabT971 <gabriluffy@gmail.com>
2024-04-23 11:41:30 +0200

    [client] filters

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,14 +1,18 @@
 
 
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref, watch } from 'vue';
 
 //datasets
 
 const items = ref([])
 const itemsPerPage = ref ("10")
 const totalItems = ref ("79")
-const search = ref ("")
+const searchName = ref("")
+const searchAtt = ref("")
+const searchConcept=ref("")
+const minCardinality = ref("0")
+const maxCardinality = ref("251734")
 const loading=ref(false)
 const headers=ref([
   {title: "id", key: "id"},
@@ -31,16 +35,24 @@ const search2 = ref ("")
 const loading2=ref(false)
 
 // function to update datasets
-
+let lastParams: any
+function updateFilter(){
+  return updateData(lastParams)
+}
 async function updateData({page, sortBy}) {
+  lastParams = {page, sortBy}
   loading.value= true
   if(sortBy.length == 0)
     sortBy = [{key: "", order: "ASC"}]
+ 
   
-  const res = await fetch("http://192.168.255.6:8080/v1/dataset?" + new URLSearchParams({
+  const res = await fetch("https://events-logs.loca.lt/v1/dataset?" + new URLSearchParams({
     limit: itemsPerPage.value,
     from: ((page-1)*Number(itemsPerPage.value)+1).toString(), 
-    searchName: search.value, 
+    searchName: searchName.value,
+    searchNameAttribute: searchAtt.value,
+    minCardinality: minCardinality.value,
+    maxCardinality: maxCardinality.value,
     sortBy: sortBy[0].key, 
     orderBy: sortBy[0].order.toUpperCase()
   }))
@@ -53,7 +65,7 @@ async function updateData({page, sortBy}) {
 }
 
 function openDialog(datasetId: number) {
-  console.log(datasetId);
+  
   
   dialog.value = true
   items2.value = []
@@ -61,16 +73,22 @@ function openDialog(datasetId: number) {
 }
 
 // function to update attributes
+let lastParams2:any
+function updateFilterAtt(){
+  return updateAttribute(lastParams2)
+}
 
 async function updateAttribute({page, sortBy}) {
+  lastParams2 = {page, sortBy}
   loading2.value= true
   if(sortBy.length == 0)
     sortBy = [{key: "", order: "ASC"}]
   
-  const res = await fetch(`http://192.168.255.6:8080/v1/dataset/${currentDatasetId.value}/attributes?` + new URLSearchParams({
+  const res = await fetch(`https://events-logs.loca.lt/v1/dataset/${currentDatasetId.value}/attributes?` + new URLSearchParams({
     limit: itemsPerPage2.value,
     from: ((page-1)*Number(itemsPerPage2.value)+1).toString(), 
-    searchName: search2.value, 
+    searchType: searchAtt.value, 
+    searchConcept: searchConcept.value,
     sortBy: sortBy[0].key, 
     orderBy: sortBy[0].order.toUpperCase()
   }))
@@ -81,31 +99,36 @@ async function updateAttribute({page, sortBy}) {
 
   
 }
-
 </script>
 
 <template>
+  
   <div>
-    <v-data-table-server
+    <div class="d-flex">
+      <v-text-field v-model="searchName" @update:modelValue="updateFilter" class="ma-0" density="compact" placeholder="Search name..." hide-details>
+      </v-text-field>
+      <v-text-field v-model="searchAtt" @update:modelValue="updateFilter" class="ma-0" density="compact" placeholder="Search attribute..." hide-details>
+      </v-text-field>
+      
+      <v-text-field type="number"  min="0" step="1" :max="maxCardinality" v-model="minCardinality" @update:modelValue="updateFilter" class="ma-0" density="compact" placeholder="Min. Cardinality" hide-details>
+      </v-text-field>
+      <v-text-field type="number" :min="minCardinality" max="251734" step="1" v-model="maxCardinality" @update:modelValue="updateFilter" class="ma-0" density="compact" placeholder="Max. Cardinality" hide-details>
+      </v-text-field>
+      
+    </div>
+  
+  <v-data-table-server
     :items="items"
     :items-length="totalItems"
-    :search="search"
     :loading="loading"
     :headers="headers"
     v-model:items-per-page="itemsPerPage"
     @update:options="updateData"
   >
-  <template v-slot:thead>
-    <tr>
-      <td></td>
-      <td>
-        <v-text-field v-model="search" class="ma-0" density="compact" placeholder="Search name..." hide-details></v-text-field>
-      </td>
-    </tr>
-  </template>
+  
     <template v-slot:item.action="{item}">
     
-      <v-btn @click="openDialog(item.id)">...</v-btn>
+      <v-btn icon @click="openDialog(item.id)">...</v-btn>
     </template>
   </v-data-table-server>
 
@@ -115,17 +138,22 @@ async function updateAttribute({page, sortBy}) {
   
   max-width="800"
   transition="dialog-transition"
-><v-card><v-data-table-server
+><v-card>
+  <div class="d-flex">
+  <v-text-field v-model="searchAtt" @update:modelValue="updateFilterAtt" class="ma-0" density="compact" placeholder="Search type..." hide-details></v-text-field>
+  <v-text-field v-model="searchConcept" @update:modelValue="updateFilterAtt" class="ma-0" density="compact" placeholder="Search name..." hide-details></v-text-field>
+  </div>
+  <v-data-table-server
     :items="items2"
     :items-length="totalItems2"
-    :search="search2"
+    :search="searchAtt"
     :loading="loading2"
     v-model:items-per-page="itemsPerPage2"
     @update:options="updateAttribute">
   <template v-slot:thead>
     <tr>
       <td>
-        <v-text-field v-model="search2" class="ma-0" density="compact" placeholder="Search name..." hide-details></v-text-field>
+
       </td>
     </tr>
   </template>
